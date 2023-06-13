404d1276f4400f77bb454bacdcbfa81a0654ab25
Isha Jasani <98201829+ishajasani@users.noreply.github.com>
2023-06-13 23:54:16 +0530

    dr

--- src/components/News.js
+++ src/components/News.js
@@ -12,29 +12,36 @@ export class News extends Component {
   }
   async componentDidMount() {
     let url =
-      "https://newsapi.org/v2/top-headlines?country=us&apiKey=84c57124174f4a51ac8f4ef508c09e40&page=1";
+      "https://newsapi.org/v2/top-headlines?country=us&apiKey=84c57124174f4a51ac8f4ef508c09e40&page=1pageSize=20";
     let data = await fetch(url);
     let parsedData = await data.json();
-    this.setState({ articles: parsedData.articles });
+    this.setState({
+      articles: parsedData.articles,
+      totalResults: parsedData.totalResults,
+    });
   }
   handlePrevClick = async () => {
-    let url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=84c57124174f4a51ac8f4ef508c09e40&page=${this.state.page - 1}`;
-  let data = await fetch(url);
-  let parsedData = await data.json();
+    let url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=84c57124174f4a51ac8f4ef508c09e40&page=${
+      this.state.page - 1
+    }&pageSize=20`;
+    let data = await fetch(url);
+    let parsedData = await data.json();
     this.setState({
-      page : this.state.page - 1,
-      articles: parsedData.articles
-    })
-  }
- handleNextClick = async () => {
-    let url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=84c57124174f4a51ac8f4ef508c09e40&page=${this.state.page +1}`;
-  let data = await fetch(url);
-  let parsedData = await data.json();
-    this.setState({
-      page : this.state.page + 1,
-      articles: parsedData.articles
-    })
-  }
+      page: this.state.page - 1,
+      articles: parsedData.articles,
+    });
+  };
+  handleNextClick = async () => {
+      let url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=84c57124174f4a51ac8f4ef508c09e40&page=${
+        this.state.page + 1
+      }&pageSize=20`;
+      let data = await fetch(url);
+      let parsedData = await data.json();
+      this.setState({
+        page: this.state.page + 1,
+        articles: parsedData.articles,
+      });
+  };
   render() {
     return (
       <div className="container my - 3 mx - 3">
@@ -59,12 +66,17 @@ export class News extends Component {
           <button
             disabled={this.state.page <= 1}
             type="button"
-            class="btn btn-dark"
+            className="btn btn-dark"
             onClick={this.handlePrevClick}
           >
             &larr; Previous
           </button>
-          <button type="button" class="btn btn-dark" onClick={this.handleNextClick}>
+          <button
+            disabled={this.state.page + 1 > Math.ceil(this.state.totalResults / 20)}
+            type="button"
+            className="btn btn-dark"
+            onClick={this.handleNextClick}
+          >
             {" "}
             Next &rarr;
           </button>
